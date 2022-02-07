21221e715c018e68e23c4bb3a4f5aa0d8304989e
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2022-02-07 16:01:38 +0600

    Set project with spring boot

--- src/main/java/com/sql/tsql/TSQLApplication.java
+++ src/main/java/com/sql/tsql/TSQLApplication.java
@@ -2,14 +2,13 @@ package com.sql.tsql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-public class Tsql2Application {
+public class TSQLApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Tsql2Application.class, args);
+        SpringApplication.run(TSQLApplication.class, args);
     }
 
 }
